5d719e4e4d7383b1b6af231a9332bfda3b9fd6ef
YuriiBalandyuk <juraitclash@gmail.com>
2024-02-20 13:48:06 +0200

    First LabWork

---
+++ src/main/java/LabWork/Main.java
@@ -0,0 +1,19 @@
+package LabWork;
+/**
+ * The main class
+ */
+public class Main {
+    /**
+     * This is the default constructor
+     */
+    public Main(){
+    }
+    /**
+     * The main method for running the application
+     * @param args Tape array
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
